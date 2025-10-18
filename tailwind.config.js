a26dc5abfcf2155f37e3fd045ebdd1e276f7a8af
madihanazir <madihan541@gmail.com>
2025-10-18 23:30:26 +0530

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+  extend: {
+    colors: {
+      primary: '#3B82F6',
+      secondary: '#E5ECF6',
+      borderLight: '#E5E7EB',
+      textDark: '#1C1C1C',
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    boxShadow: {
+      card: '0px 1px 3px rgba(0, 0, 0, 0.08)',
+    },
   },
+},
+
   plugins: [],
 }
 
